1e736047533f35755a367ad230c5b41ebeef12f3
KenMac <armstrong17.km@gmail.com>
2025-07-12 21:47:22 +0800

    Update below list: 	configserver endpoint 	profile active 	swagger description 	client intraoral endpoints

--- src/main/java/com/km/docmacc/intraoralbill/IntraOralBillApplication.java
+++ src/main/java/com/km/docmacc/intraoralbill/IntraOralBillApplication.java
@@ -1,8 +1,13 @@
 package com.km.docmacc.intraoralbill;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -12,4 +17,20 @@ public class IntraOralBillApplication {
 		SpringApplication.run(IntraOralBillApplication.class, args);
 	}
 
+	@Bean
+	public GroupedOpenApi publicApi() {
+		return GroupedOpenApi.builder()
+				.group("intraoral-bill-public")
+				.pathsToMatch("/api/v1/intraOralBill/**")
+				.build();
+	}
+
+	@Bean
+	public OpenAPI maccDentalClinicOpenAPI() {
+		return new OpenAPI()
+				.info(new Info().title("IntraOral Bill API")
+						.description("IntraOral Bill API for MACC Dental Clinic")
+						.version("v1.0.0")
+						.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+	}
 }
